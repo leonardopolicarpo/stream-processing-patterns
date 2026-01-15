550fced333e009d3d538dd45b76aa6f68f560d34
Leonardo Policarpo <leo.dumont@outlook.com>
2026-01-15 15:48:01 -0300

    feat: implements sql writer

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 
 from src.adapters.csv_reader import CsvReader
-# from src.adapters.sqlite_writer import SqliteWriter
+from src.adapters.sqlite_writer import SqliteWriter
 from src.service.etl_manager import ETLManager
 
 logging.basicConfig(
@@ -15,17 +15,20 @@ logger = logging.getLogger(__name__)
 def main():
   data_dir = Path(__file__).parent.parent / "data"
   input_file = data_dir / "ledger.csv"
-  # db_file = data_dir / "finance.db"
+  db_file = data_dir / "finance.db"
 
   logger.info(f"Iniciando ETL...")
   logger.info(f"Lendo de: {input_file}")
-  # logger.info(f"Salvando em: {db_file}")
+  logger.info(f"Salvando em: {db_file}")
 
   csv_reader = CsvReader(filepath=input_file)
 
-  # sqlite_writer = SqliteWriter(db_path=db_file)
+  sqlite_writer = SqliteWriter(db_path=db_file)
 
-  etl_service = ETLManager(reader=csv_reader)
+  etl_service = ETLManager(
+    reader=csv_reader,
+    writer=sqlite_writer
+  )
 
   try:
     etl_service.run()
--- src/adapters/sqlite_writer.py
+++ src/adapters/sqlite_writer.py
@@ -1,7 +1,68 @@
+import logging
+import sqlite3
+from typing import List
+
+logger = logging.getLogger(__name__)
+
+from src.domain.transaction import Transaction
 class SqliteWriter:
   def __init__(self, db_path: str):
-    self.db_path = db_path
+    self.db_path = str(db_path)
+    self.conn = sqlite3.connect(self.db_path)
+    self._setup_db()
+
+  def _setup_db(self):
+    cursor = self.conn.cursor()
+
+    cursor.execute("PRAGMA journal_mode=WAL;")
+    cursor.execute("PRAGMA synchronous=NORMAL;")
+
+    cursor.execute("""
+      CREATE TABLE IF NOT EXISTS transactions (
+        id TEXT PRIMARY KEY,
+        sender TEXT,
+        receiver TEXT,
+        amount TEXT,
+        fee TEXT,
+        currency TEXT,
+        network TEXT,
+        status TEXT,
+        timestamp TEXT
+      )
+    """)
+    self.conn.commit()
   
-  def write_batch(self):
-    with open(self.filepath, ...) as f:
-      ...
+  def write_batch(self, batch: List[Transaction]) -> None:
+    if not batch:
+      return
+    
+    data_to_insert = [
+      (
+        str(t.id),
+        t.sender,
+        t.receiver,
+        str(t.amount),
+        str(t.fee),
+        t.currency,
+        t.network,
+        t.status,
+        t.timestamp.isoformat()
+      )
+      for t in batch
+    ]
+
+    cursor = self.conn.cursor()
+    try:
+      sql = """
+        INSERT INTO transactions
+        (id, sender, receiver, amount, fee, currency, network, status, timestamp)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+      """
+      cursor.executemany(sql, data_to_insert)
+    except Exception as e:
+      self.conn.rollback()
+      logger.error(f"Erro ao salvar batch: {e}")
+      raise e
+  
+  def close(self):
+    self.conn.close()
